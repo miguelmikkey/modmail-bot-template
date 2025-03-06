1b488a9d4001a26703f0a33ae47d5e52c6514b0e
miguelmikkey <mangelgonzalezmartinez@gmail.com>
2025-03-06 11:00:15 +0000

    modified config.js and .env

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,10 @@ module.exports = {
   GUILD_ID: process.env.GUILD_ID,
   BOT_TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
+
+  modmail: {
+    categoryID: process.env.MODMAIL_CATEGORY_ID,
+    logChannelID: process.env.MODMAIL_LOG_CHANNEL_ID,
+    staffRoles: process.env.MODMAIL_ROLE_ID,
+  },
 };
